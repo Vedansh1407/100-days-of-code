1813b73764785d923dc12b57b5de5926f49a4bdc
Vedansh1407 <Vedansh.42489@stu.ac.in>
2026-09-22 11:08:09 +0530

    day 2

--- Hello.c
+++ Hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+int num1, num2;
 int main() {
-    printf("Hello, World!\n");
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Sum: %d\n", num1 + num2);
+    printf("Difference: %d\n", num1 - num2);
+    printf("Product: %d\n", num1 * num2);
+    printf("Quotient: %.2f\n", (float)num1 / num2);
     return 0;
-}    
+}
